bf6a0a1ace8394d378d68746638ffa057e1035a9
tmuellersim <tmuellersim@gmail.com>
2016-12-10 22:53:22 -0500

    Automatically extracting edges using Hough transform and K-means clustering
    Also uploaded output of lines into corresponding .mat files. Images which failed to detect lines were 8, 16, 18, 22.

--- main_Scan.m
+++ main_Scan.m
@@ -1,13 +1,13 @@
-% clearvars
-clear
+clearvars
+% clear
 close all
 clc
 
 %% Parameters
 intThresh = 220; % Threshold for intensity values, below which all values are mapped to some color, integer from 1-255
-scanFilename = './scan_data/scan2.txt';
-imgFilename = '/image_data/scan2.jpg';
-manualLines = 1;  % Manually trace the lines in the image if set to 1, automatic line extraction if 0
+scanFilename = './scan_data/scan7.txt';
+imgFilename = './image_data/calibrationImage0007.png';
+manualLines = 0;  % Manually trace the lines in the image if set to 1, automatic line extraction if 0
 manualPoints = 1;  % Manually specify points in the laser scan corresponding to edges of the calibration marker
 
 %% Import data from files.
@@ -16,7 +16,7 @@ scanData = loadScanData(scanDataCell);  % returns structure scanData
 % displayScanData(scanData, intThresh);  % Display scan data
 
 img = imread(imgFilename);
-img = imrotate(img, -90);
+% img = imrotate(img, -90);
 figure
 imshow(img);
 
@@ -27,10 +27,23 @@ scanPoints = extractPoints(scanData, intThresh, manualPoints);
 
 %% Find lines in image
 imgLines = extractEdges(img, manualLines);
+save(strcat(imgFilename(1:(end-4)), '.mat'), imgLines);
 % save imgLines.mat imgLines;
 % load imgLines.mat;
+% Test calibration images
+close all
+for i = 0:22
+    if i < 10
+        imgFilename = strcat('./image_data/calibrationImage000', num2str(i), '.png');
+    else
+        imgFilename = strcat('./image_data/calibrationImage00', num2str(i), '.png');
+    end
+    img = imread(imgFilename);
+    imgLines = extractEdges(img, manualLines);
+    saveas(gcf, strcat(imgFilename(1:(end-4)), '-FIT_LINES.jpg'))
+    save(strcat(imgFilename(1:(end-4)), '.mat'), 'imgLines');
+end
 
 %% Error Minimization using Levenberg-Marquardt
-
-mappingUsingInitialVals(img, imgLines, scanPoints);
+% mappingUsingInitialVals(img, imgLines, scanPoints);
 
